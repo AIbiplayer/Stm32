20f1992bf247280f845c8199389442a94fddd4ef
AIbiplayer <1805907583@qq.com>
2025-10-23 21:43:46 +0800

    步兵框架初始化

---
+++ MDK-ARM/Omni-Framework/usbd_cdc.d
@@ -0,0 +1,47 @@
+omni-framework\usbd_cdc.o: ../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c
+omni-framework\usbd_cdc.o: ../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h
+omni-framework\usbd_cdc.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h
+omni-framework\usbd_cdc.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h
+omni-framework\usbd_cdc.o: ../USB_DEVICE/Target/usbd_conf.h
+omni-framework\usbd_cdc.o: F:\Keil 5\ARM\ARMCC\Bin\..\include\stdio.h
+omni-framework\usbd_cdc.o: F:\Keil 5\ARM\ARMCC\Bin\..\include\stdlib.h
+omni-framework\usbd_cdc.o: F:\Keil 5\ARM\ARMCC\Bin\..\include\string.h
+omni-framework\usbd_cdc.o: ../Core/Inc/main.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+omni-framework\usbd_cdc.o: ../Core/Inc/stm32f4xx_hal_conf.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
+omni-framework\usbd_cdc.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+omni-framework\usbd_cdc.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
+omni-framework\usbd_cdc.o: ../Drivers/CMSIS/Include/core_cm4.h
+omni-framework\usbd_cdc.o: F:\Keil 5\ARM\ARMCC\Bin\..\include\stdint.h
+omni-framework\usbd_cdc.o: ../Drivers/CMSIS/Include/cmsis_version.h
+omni-framework\usbd_cdc.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+omni-framework\usbd_cdc.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+omni-framework\usbd_cdc.o: ../Drivers/CMSIS/Include/mpu_armv7.h
+omni-framework\usbd_cdc.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+omni-framework\usbd_cdc.o: F:\Keil 5\ARM\ARMCC\Bin\..\include\stddef.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_can.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rng.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h
+omni-framework\usbd_cdc.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h
+omni-framework\usbd_cdc.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h
+omni-framework\usbd_cdc.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h
+omni-framework\usbd_cdc.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h
